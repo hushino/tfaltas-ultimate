8b5e668a22c5836c1b81588c7e80175efba8f79f
hushino <frostshock@outlook.com>
2020-06-04 10:25:33 -0300

    f

--- personas/ingper.php
+++ personas/ingper.php
@@ -200,18 +200,20 @@ $totalRows_oppago = mysql_num_rows($oppago);
     <!-- Contenido (inicio) -->
 
     <div class="row">
+
       <form class="text-center p-5" name="formulario" id="formulario" action="<?php echo $editFormAction; ?>"
         method="POST">
         <legend class="ui-widget-header ui-corner-all ui-state-highlight" style="padding:1px 10px;" id="grupoContacto">
           Datos de la Falta Cometida
         </legend>
         <div class="col">
-          <label for="basic-url">Seleccionar Dependencia de Origen:</label>
-          <div class="input-group mb-3">
-            <select class="browser-default custom-select" name="deporig" id="deporig">
-              <?php do { ?>
-              <option value="<?php echo $row_depend['IDC'] ?>"><?php echo $row_depend['DETALLES'] ?></option>
-              <?php
+          <div class="col">
+            <label for="basic-url">Seleccionar Dependencia de Origen:</label>
+            <div class="input-group mb-3">
+              <select class="browser-default custom-select" name="deporig" id="deporig">
+                <?php do { ?>
+                <option value="<?php echo $row_depend['IDC'] ?>"><?php echo $row_depend['DETALLES'] ?></option>
+                <?php
           } while ($row_depend = mysql_fetch_assoc($depend));
           $rows = mysql_num_rows($depend);
           if ($rows > 0) {
@@ -219,62 +221,62 @@ $totalRows_oppago = mysql_num_rows($oppago);
               $row_depend = mysql_fetch_assoc($depend);
           }
           ?>
-            </select>
-          </div>
-        </div>
-        <div class="col">
-          <label for="basic-url">Acta de Inf. Nº</label>
-          <div class="input-group mb-3">
-            <input name="infacta" required="required" autofocus id="infacta" type="text" required="required"
-              id="infacta" class="form-control" aria-describedby="basic-addon3" onFocusOut="javascript:verificar()"
-              autocomplete="off" />
-            <div id="final"></div>
-          </div>
-        </div>
-        <div class="col">
-          <label for="basic-url">Nº de expediente</label>
-          <div class="input-group mb-3">
-            <input class="form-control" name="IDx" required="required" autofocus type="text" required="required"
-              id="IDxx" />
-          </div>
-        </div>
-        <div class="form-row mb-4">
-          <div class="col">
-            <label for="basic-url">Fecha</label>
-            <div class="input-group">
-              <input class="form-control" name="inffecha" type="date" required="required" id="infdate" />
+              </select>
             </div>
           </div>
           <div class="col">
-            <label for="basic-url">Hora</label>
-            <div class="input-group ">
-              <input class="form-control" name="infhora" type="time" required="required" id="infhora"
-                autocomplete="off" />
-            </div>
-          </div>
-        </div>
-
-        <div class="col">
-          <label for="basic-url">Lugar en que se cometió la falta</label>
-          <div class="input-group mb-3">
-            <input class="form-control" name="influg" type="text" required="required" id="influg" />
-          </div>
-        </div>
-        <div class="col">
-          <label for="basic-url">Infracción</label>
-          <div class="input-group mb-3">
-            <input class="form-control" name="infart" type="text" id="infartx" required="required" />
-          </div>
-        </div>
-
-        <div class="form-row mb-4">
-          <div class="col">
-            <label for="basic-url">Datos del Vehículo:</label>
+            <label for="basic-url">Acta de Inf. Nº</label>
             <div class="input-group mb-3">
-              <select class="browser-default custom-select" name="infvehi" id="infvehi">
-                <?php do { ?>
-                <option value="<?php echo $row_opvehi['IDC'] ?>"><?php echo $row_opvehi['DETALLES'] ?></option>
-                <?php
+              <input name="infacta" required="required" autofocus id="infacta" type="text" required="required"
+                id="infacta" class="form-control" aria-describedby="basic-addon3" onFocusOut="javascript:verificar()"
+                autocomplete="off" />
+              <div id="final"></div>
+            </div>
+          </div>
+          <div class="col">
+            <label for="basic-url">Nº de expediente</label>
+            <div class="input-group mb-3">
+              <input class="form-control" name="IDx" required="required" autofocus type="text" required="required"
+                id="IDxx" />
+            </div>
+          </div>
+          <div class="form-row mb-4">
+            <div class="col">
+              <label for="basic-url">Fecha</label>
+              <div class="input-group">
+                <input class="form-control" name="inffecha" type="date" required="required" id="infdate" />
+              </div>
+            </div>
+            <div class="col">
+              <label for="basic-url">Hora</label>
+              <div class="input-group ">
+                <input class="form-control" name="infhora" type="time" required="required" id="infhora"
+                  autocomplete="off" />
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <label for="basic-url">Lugar en que se cometió la falta</label>
+            <div class="input-group mb-3">
+              <input class="form-control" name="influg" type="text" required="required" id="influg" />
+            </div>
+          </div>
+          <div class="col">
+            <label for="basic-url">Infracción</label>
+            <div class="input-group mb-3">
+              <input class="form-control" name="infart" type="text" id="infartx" required="required" />
+            </div>
+          </div>
+
+          <div class="form-row mb-4">
+            <div class="col">
+              <label for="basic-url">Datos del Vehículo:</label>
+              <div class="input-group mb-3">
+                <select class="browser-default custom-select" name="infvehi" id="infvehi">
+                  <?php do { ?>
+                  <option value="<?php echo $row_opvehi['IDC'] ?>"><?php echo $row_opvehi['DETALLES'] ?></option>
+                  <?php
                              } while ($row_opvehi = mysql_fetch_assoc($opvehi));
                                     $rows = mysql_num_rows($opvehi);
                                     if ($rows > 0) {
@@ -282,33 +284,33 @@ $totalRows_oppago = mysql_num_rows($oppago);
                                         $row_opvehi = mysql_fetch_assoc($opvehi);
                              }
                     ?>
-              </select>
+                </select>
+              </div>
+            </div>
+            <div class="col">
+              <label for="basic-url">Dominio</label>
+              <div class="input-group mb-3">
+                <input class="form-control" name="infautopat" type="text" id="infautopat"
+                  title="sólo 7 letras o números. Las letras en mayúsculas" maxlength="7"
+                  onkeyup="javascript:this.value=this.value.toUpperCase();" autocomplete="off" />
+              </div>
             </div>
           </div>
           <div class="col">
-            <label for="basic-url">Dominio</label>
-            <div class="input-group mb-3">
-              <input class="form-control" name="infautopat" type="text" id="infautopat"
-                title="sólo 7 letras o números. Las letras en mayúsculas" maxlength="7"
-                onkeyup="javascript:this.value=this.value.toUpperCase();" autocomplete="off" />
+            <label for="basic-url">Oblada</label>
+            <div class="custom-control custom-checkbox">
+              <input value="S" onchange="javascript:showContent()" name="oblada" type="checkbox" id="oblada"
+                class="custom-control-input" title="Marcar si surge pago espontáneo del acta">
+              <label class="custom-control-label" for="oblada">Marcar si surge pago espontáneo del
+                acta</label>
             </div>
-          </div>
-        </div>
-        <div class="col">
-          <label for="basic-url">Oblada</label>
-          <div class="custom-control custom-checkbox">
-            <input value="S" onchange="javascript:showContent()" name="oblada" type="checkbox" id="oblada"
-              class="custom-control-input" title="Marcar si surge pago espontáneo del acta">
-            <label class="custom-control-label" for="oblada">Marcar si surge pago espontáneo del
-              acta</label>
-          </div>
-          <div class="w-100"></div>
-          <div id="contoblada" style="display: none;">
-            <div class="col">
-              <select class="browser-default custom-select" name="infpago" id="infpago">
-                <?php do { ?>
-                <option value="<?php echo $row_oppago['IDC'] ?>"><?php echo $row_oppago['DETALLES'] ?></option>
-                <?php
+            <div class="w-100"></div>
+            <div id="contoblada" style="display: none;">
+              <div class="col">
+                <select class="browser-default custom-select" name="infpago" id="infpago">
+                  <?php do { ?>
+                  <option value="<?php echo $row_oppago['IDC'] ?>"><?php echo $row_oppago['DETALLES'] ?></option>
+                  <?php
                       } while ($row_oppago = mysql_fetch_assoc($oppago));
                       $rows = mysql_num_rows($oppago);
                       if ($rows > 0) {
@@ -316,17 +318,159 @@ $totalRows_oppago = mysql_num_rows($oppago);
                       $row_oppago = mysql_fetch_assoc($oppago);
                      }
                    ?>
-              </select>
+                </select>
+              </div>
+              <div class="col">
+                <label for="basic-url">Recibo</label>
+                <div class="input-group">
+                  <input class="form-control" name="obladarec" type="text" id="obladarec"
+                    title="Ingresar el Número de Recibo." />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="custom-control custom-checkbox">
+              <input class="custom-control-input" name="retlice" type="checkbox" id="retlice" value="S" />
+              <label class="custom-control-label" for="retlice">Retención Licencia</label>
+            </div>
+          </div>
+          <div class="col">
+            <div class="custom-control custom-checkbox">
+              <input class="custom-control-input" name="retvehi" type="checkbox" id="retvehi" value="S" />
+              <label class="custom-control-label" for="retvehi">Retención de Vehículo</label>
+            </div>
+          </div>
+
+
+          <div class="col">
+            <label for="basic-url">Observaciones</label>
+            <div class="input-group">
+              <input class="form-control" name="infobser" type="text" id="infobser" />
+            </div>
+          </div>
+        </div>
+
+        <legend class="ui-widget-header ui-corner-all ui-state-highlight" style="padding:1px 10px;" id="grupoContacto">
+          Datos del Infractor
+        </legend>
+        <div class="col">
+          <legend class="ui-widget-header ui-corner-all ui-state-highlight" style="padding:1px 10px;"
+            id="grupoContacto">
+            Datos Personales
+          </legend>
+          <div class="form-row mb-4">
+            <div class="col">
+              <label for="basic-url">Apellidos</label>
+              <div class="input-group">
+                <input class="form-control" autocomplete="off" name="apellidos" type="text" id="apellido" />
+              </div>
             </div>
             <div class="col">
-              <label for="basic-url">Recibo</label>
+              <label for="basic-url">Nombres</label>
               <div class="input-group">
-                <input class="form-control" name="obladarec" type="text" id="obladarec"
-                  title="Ingresar el Número de Recibo." />
+                <input class="form-control" autocomplete="off" name="nombres" type="text" id="nombres" />
+              </div>
+            </div>
+          </div>
+          <div class="form-row mb-4">
+            <div class="col">
+              <label for="basic-url">D.N.I.</label>
+              <div class="input-group">
+                <input class="form-control" autocomplete="off" name="dni" type="text" id="dni"
+                  onFocusOut="javascript:verificardni()" />
+              </div>
+            </div>
+            <div class="col">
+              <label for="basic-url">Doc.Nac.</label>
+              <div class="input-group">
+                <input class="form-control" autocomplete="off" pattern="[A-Z]{3}" value="ARG" name="dninac" type="text"
+                  id="dninac" />
               </div>
             </div>
           </div>
         </div>
+
+
+
+        <div class="col">
+          <legend class="ui-widget-header ui-corner-all ui-state-highlight" style="padding:1px 10px;"
+            id="grupoContacto">
+            Domicilio
+          </legend>
+
+
+
+          <div class="form-row mb-4">
+            <div class="col">
+              <label for="basic-url">Calle o Barrio</label>
+              <div class="input-group">
+                <input class="form-control" name="dirCalle" type="text" id="dirCalle" />
+              </div>
+            </div>
+            <div class="col">
+              <label for="basic-url">Nro.</label>
+              <div class="input-group">
+                <input class="form-control" name="dirNro" type="text" id="dirNro" />
+              </div>
+            </div>
+            <div class="col">
+              <label for="basic-url">Piso</label>
+              <div class="input-group">
+                <input class="form-control" name="dirPiso" type="text" id="dirPiso" />
+              </div>
+            </div>
+          </div>
+
+          <div class="form-row mb-4">
+            <div class="col">
+              <label for="basic-url">Dpto.</label>
+              <div class="input-group">
+                <input class="form-control" name="dirDpto" type="text" id="dirDpto" />
+              </div>
+            </div>
+            <div class="col">
+              <label for="basic-url">Mz.</label>
+              <div class="input-group">
+                <input class="form-control" name="dirMz" type="text" id="dirMz" />
+              </div>
+            </div>
+            <div class="col">
+              <label for="basic-url">Casa</label>
+              <div class="input-group">
+                <input class="form-control" name="dirCs" type="text" id="dirCs" />
+              </div>
+            </div>
+          </div>
+
+          <div class="form-row mb-4">
+            <div class="col">
+              <label for="basic-url">Mbk.</label>
+              <div class="input-group">
+                <input class="form-control" name="dirMbk" type="text" id="dirMbk" />
+              </div>
+            </div>
+            <div class="col">
+              <label for="basic-url">Localidad</label>
+              <div class="input-group">
+                <input class="form-control" value="Clorinda" name="localidad" type="text" id="localidad" />
+              </div>
+            </div>
+            <div class="col">
+              <label for="basic-url">Provincia</label>
+              <div class="input-group">
+                <input class="form-control" type="hidden" name="idProvincia" id="idProvincia" />
+                <input class="form-control" name="descProvincia" type="text" id="descProvincia" style="width:200px;"
+                  tabindex="20" value="Formosa" />
+              </div>
+            </div>
+          </div>
+
+
+
+        </div>
+
 
         <button type="button" name="button" id="button" onclick="history.back()"
           class="btn btn-danger btn-rounded">Volver
@@ -337,6 +481,9 @@ $totalRows_oppago = mysql_num_rows($oppago);
         </button>
         <input type="hidden" name="MM_insert" value="formulario">
       </form>
+
+
+
     </div>
 
     <div id="div_formulario">
